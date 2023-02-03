1def6089e20df34316fbc8cb376625cc432363d6
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-02-03 12:18:31 -0700

    finished the UI for earth

--- CSE335Lab2/EarthView.swift
+++ CSE335Lab2/EarthView.swift
@@ -12,42 +12,48 @@ struct EarthView: View {
     // This variable is responsible for tracking the data sent from SecondView
     @State private var dataFromMoon = "";
     @State var weight: String = "";
-
+    
     var body: some View {
         // NOTE: to navigate between views, the root view needs to be embedded
         // inside a NavigationView
-            
-            NavigationView {
-                VStack(spacing:50){
-                    VStack(spacing:30){
-                        Text("Welcome to Space Walk App");
-                        
-                        Text("You are on earth now");
-                    }
+        
+        NavigationView{
+            VStack(spacing:75){
+                VStack(spacing:30){
+                    Text("Welcome to Space Walk App")
+                    
+                    Text("You are on earth now")
+                }
+                HStack{
+                    Text("Enter your weight: ");
+                    TextField("", text: $weight).keyboardType(.decimalPad) .textFieldStyle(.roundedBorder);
+                }
+                VStack {
                     Spacer()
-                    HStack{
-                        Text("Enter your weight: ");
-                        TextField("", text: $weight).keyboardType(.decimalPad);
+                    Image("Earth").resizable().aspectRatio(contentMode: .fit).frame(height: 150)
+                    Spacer()
+                    NavigationLink("Go to Moon") {
+                        MoonView(
+                            dataFromEarth: "Hello from ContentView",
+                            dataMoon: $dataFromMoon
+                            // Binding
+                            
+                        )
+                    }.buttonStyle(.borderedProminent)
+                    .navigationTitle("ContentView")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .onChange(of: dataFromMoon) { data in
+                        print(data)
+                        dataFromMoon = ""
                     }
-                        
-                        
-                        Image("Earth").resizable().aspectRatio(contentMode: .fit).frame(height: 150)
-                        NavigationLink("Go to Moon") {
-                            MoonView(
-                                dataFromEarth: "Hello from ContentView",
-                                dataMoon: $dataFromMoon
-                                // Binding
-                                
-                            )
-                        }
-                        .onChange(of: dataFromMoon) { data in
-                            print(data)
-                            dataFromMoon = ""
-                        }
-                    }
-            }
+                }
+                .navigationBarHidden(true)
+    
+            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+
         }
     }
+}
 
 struct EarthView_Previews: PreviewProvider {
     static var previews: some View {
