ac8f711303912006b3f7e1f96eab9236b3733b64
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-02-03 13:24:59 -0700

    made the UI for jupiter

--- CSE335Lab2/JupiterView.swift
+++ CSE335Lab2/JupiterView.swift
@@ -18,17 +18,41 @@ struct JupiterView: View {
     @Binding var dataJupiter: Int
     
     @State var comeToJupiter: Int = 0
-
+    
     
     var body: some View {
-        Text("\(weightEarth)");
-        Text("\(weightMoon)");
-        Text("\(weightJupiter)");
+        
+        
+        VStack{
+            VStack {
+                Text("You are on the jupiter");
+                Image("Jupiter").resizable().aspectRatio(contentMode: .fit).frame(height: 150)
+            }
+            
+            
+            VStack {
+                Text("Your weight on Earth is... \(weightEarth)");
+                Text("Your weight on Moon is... \(weightMoon)");
+                Text("Your weight on Jupiter is... \(weightJupiter)");
+            }
+            
+            VStack {
+                Text("I feel much heavier !")
+                Spacer()
+                Button("Go to Earth")
+                {
+                    dataJupiter = 2
+                    dismiss()
+                }.padding().foregroundColor(.blue).cornerRadius(10)
+            }
+        }.navigationBarHidden(true)
+            .navigationTitle("Jupiter")
+            .navigationBarTitleDisplayMode(.inline)
     }
-}
-
-struct JupiterView_Previews: PreviewProvider {
-    static var previews: some View {
-        JupiterView(weightEarth: 0.0, weightMoon: 0.0, dataJupiter: .constant(2))
+    
+    struct JupiterView_Previews: PreviewProvider {
+        static var previews: some View {
+            JupiterView(weightEarth: 0.0, weightMoon: 0.0, dataJupiter: .constant(2))
+        }
     }
 }
